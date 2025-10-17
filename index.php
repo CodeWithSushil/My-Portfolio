e6e06b805dac415144f7b891011e7a339f9a46e5
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-10-17 16:18:24 +0530

    add header(landing page)

--- index.php
+++ index.php
@@ -1,34 +1,23 @@
-<?php
-$db = new PDO('sqlite:database/portfolio.sqlite');
-$articles = $db->query('SELECT * FROM articles ORDER BY created_at DESC')->fetchAll(PDO::FETCH_ASSOC);
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>My Portfolio</title>
-  <link rel="stylesheet" href="assets/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="My Portfolio">
+    <meta name="robots" content="index, follow">
+    <link rel="icon" href="favicon.ico">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/1.0.4/css/bulma.min.css" integrity="sha512-yh2RE0wZCVZeysGiqTwDTO/dKelCbS9bP2L94UvOFtl/FKXcNAje3Y2oBg/ZMZ3LS1sicYk4dYVGtDex75fvvA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
-<body>
-  <header>
-    <h1>Welcome to My Portfolio</h1>
-    <nav>
-      <a href="/">Home</a>
-      <a href="./pages/about.php">About</a>
-      <a href="./pages/articles.php">Articles</a>
-      <a href="./contact.php">Contact</a>
-    </nav>
-  </header>
-  <main>
-    <h2>Latest Articles</h2>
-    <ul>
-      <?php foreach($articles as $a): ?>
-        <li><strong><?= htmlspecialchars($a['title']) ?></strong> — <?= htmlspecialchars($a['created_at']) ?></li>
-      <?php endforeach; ?>
-    </ul>
-  </main>
-<script>
-console.log("Welcome to my Portfolio!");
-</script>
+<body class="is-family-primary">
+    <!--header-->
+    <h2 class="mt-4 is-size-4-mobile is-size-3-tablet is-size-2-desktop is-size-1-widescreen has-text-link has-text-centered">Welcome to My Portfolio</h2>
+    <p class="m-4 has-text-centered is-size-6-mobile is-size-5-desktop">
+        Hello Developers, <span class="has-text-success"> I'm Sushil Kumar</span> aka <span class="has-text-danger">Code With Sushil</span>.
+    </p>
+    <!--header-->
+
 </body>
 </html>
