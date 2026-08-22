0214c30e44b52acfa4066a51a4608b7bbe44e8b6
Sushil Kumar <sushilkumbhar77@gmail.com>
2026-08-22 20:32:48 +0530

    sushilkumar move to ci4

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'sushilkumar77@gmail.com';
+    public string $fromName   = 'Sushil Kumar';
     public string $recipients = '';
 
     /**
