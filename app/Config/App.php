d0f5afa908e041b299125f703b2be6e06d1aae60
Sushil Kumar <sushilkumbhar77@gmail.com>
2026-08-31 13:19:22 +0530

    updated

--- app/Config/App.php
+++ app/Config/App.php
@@ -6,5 +6,12 @@ namespace App\Config;
 
 class App
 {
-    //
+    ini_set('display_errors', '0');
+    ini_set('display_startup_errors', '0');
+    ini_set('log_errors', '1');
+    error_reporting(E_ALL);
+
+    public function __construct(){
+        echo 'App class';
+    }
 }
